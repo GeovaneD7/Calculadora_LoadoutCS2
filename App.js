03d8446270f9d4dc322f6bb99ce5060dbd6f0dbf
Geovane Dias de Araujo <38440800+GeovaneD7@users.noreply.github.com>
2024-02-01 20:12:39 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+function Equipamento(categoria, valor, imagem, lado); {
+    this.categoria = categoria;
+    this.valor = valor;
+    this.imagem = imagem;
+    this.lado = lado;
+}
+
+var = new Equipamento(, , , );
