17ed8645c4bcf0b9f14427065d99e51250aab355
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-05-09 18:33:35 -0300

    modified:   App.js 	modified:   index.html 	new file:   style.css
        todos esses arquivos serão descontinuados

--- App.js
+++ App.js
@@ -7,3 +7,16 @@
 //    }
 
 const objData = JSON.parse(Equipamentos)
+
+$(document).ready(function() {
+    // Torna as imagens arrastáveis usando jQuery UI
+    $('img').draggable();
+
+    // Define as tabelas como áreas onde as imagens podem ser soltas
+    $('.table').droppable({
+        drop: function(event, ui) {
+            // Obtém a imagem que foi solta e a move para dentro da tabela
+            $(this).append(ui.draggable);
+        }
+    });
+});
